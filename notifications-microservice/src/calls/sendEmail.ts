2bcb8e5dcbc61f0b4d3e4dcf2adc1803fe0590d1
Andres Lobaton <andrespipe021028@gmail.com>
2024-03-13 23:49:08 -0500

    feat: simulate conume api & insert DB

--- notifications-microservice/src/calls/sendEmail.ts
+++ notifications-microservice/src/calls/sendEmail.ts
@@ -1,9 +1,21 @@
-import { EmailPayload } from "../interfaces/common"
+import axios from "axios";
+import { EmailPayload } from "../interfaces/common";
 
-export default (call: any, callback: any)=>{
-    console.log(call.request as EmailPayload)
-    console.log("Loggind sending EMAIL notification to : "+call.request.email)
-    callback(null, {
-        is_error: false
-    });
+function insertDb(data: any) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("");
+      console.log("Inserted on DB: ", data.login);
+    }, 500);
+  });
 }
+
+export default async (call: any, callback: any) => {
+  const { data } = await axios.get("https://api.github.com/users/Lobaton2020");
+  await insertDb(data);
+  console.log(call.request as EmailPayload);
+  console.log("Loggind sending EMAIL notification to : " + call.request.email);
+  callback(null, {
+    is_error: false,
+  });
+};
--- orders-microservice/src/index.ts
+++ orders-microservice/src/index.ts
@@ -1,26 +1,39 @@
 import { credentials, loadPackageDefinition, Server, ServerCredentials } from '@grpc/grpc-js'
 import { loadSync } from '@grpc/proto-loader'
 
-async function bootstrap(){
-    try{
-        const packageDefinition = loadSync('./src/proto/notification.proto', {
-            keepCase: true,
-            defaults: true,
-            oneofs: true
-         });
-        const notification = loadPackageDefinition(packageDefinition).notification as any;
-        const client = new notification.Notification('0.0.0.0:50051', credentials.createInsecure())
-        client.SendSms({ name : 'Andres Lobaton' }, (error: any, response: any) =>{
-            console.log({error, response})
-        })
-        setTimeout(() => {
-            client.SendEmail({ email : 'andrespipe021028@gmail.com' }, (error: any, response: any) =>{
-                console.log({error, response})
-            })
-        }, 5000);
-    }catch(err){
-        console.log("Error inicializando ORDERS MICROSEVICE", err)
-    }
+function promiseRpc(client: any, method: string, data: any) {
+  return new Promise((resolve, reject) => {
+    client[method](data, (error: any, response: any) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(response);
+    });
+  });
+}
+async function bootstrap() {
+  try {
+    const packageDefinition = loadSync("./src/proto/notification.proto", {
+      keepCase: true,
+      defaults: true,
+      oneofs: true,
+    });
+    const notification = loadPackageDefinition(packageDefinition)
+      .notification as any;
+    const client = new notification.Notification(
+      "0.0.0.0:50051",
+      credentials.createInsecure()
+    );
+    console.time();
+    const [res1, res2] = await Promise.all([
+      promiseRpc(client, "SendSms", { name: "Andres Lobaton" }),
+      promiseRpc(client, "SendEmail", { email: "andrespipe021028@gmail.com" }),
+    ]);
+    console.timeEnd();
+    console.log({ res1, res2 });
+  } catch (err) {
+    console.log("Error inicializando ORDERS MICROSEVICE", err);
+  }
 }
 
 bootstrap()
